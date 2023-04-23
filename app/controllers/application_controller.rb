dea1c966c9461dd6654e1378f82827a5c4c1bb89
Ruiyot Abby <ruiyotabby@gmail.com>
2023-04-23 11:57:01 +0300

    Add name to the permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
+  before_action :configure_permitted_parameters
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
