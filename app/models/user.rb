8bacaf14da33e5244d662f5448136ddf3db90415
Ruiyot Abby <ruiyotabby@gmail.com>
2023-04-23 16:14:37 +0300

    Add reference to post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :email, uniqueness: true
   validates :name, presence: true
+  has_many :posts, dependent: :destroy
 end
