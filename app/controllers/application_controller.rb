92d6edcd08a251d1c84820e8457d384e9d994ef7
Ruiyot Abby <ruiyotabby@gmail.com>
2023-04-24 15:33:15 +0300

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   self.responder = ApplicationResponder
   respond_to :html
-
+  
   protected
 
   def configure_permitted_parameters
