3f1045526a7cb9643f508e272093a91b115e747c
Ruiyot Abby <ruiyotabby@gmail.com>
2023-04-24 15:07:05 +0300

    Add posts actions and params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
